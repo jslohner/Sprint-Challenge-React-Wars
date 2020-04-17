749b7b473c2d6d1d912b9be6f961216895073089
Justin Lohner <lohner4@gmail.com>
2020-04-17 10:44:55 -0600

    Used axios to get character data from api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+// import Character from './components/Character.js';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	const proxy = 'https://cors-anywhere.herokuapp.com';
+	const url = 'https://rickandmortyapi.com/api';
+	const targetData = 'character';
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+	const [characterData, setCharacterData] = useState([]);
+	const [currentCharacter, setCurrentCharacter] = useState('');
+
+	useEffect(() => {
+		axios.get(`${proxy}/${url}/${targetData}/`)
+			.then(res => {
+				// console.log(res.data.results);
+				setCharacterData(res.data.results);
+			})
+			.catch(err => {
+				console.log('error');
+			})
+	}, [])
+
+	console.log(characterData);
+
+	if(!characterData) {
+		return (
+			<h3>Data Loading</h3>
+		);
+	}
+
+	return (
+		<div className='App'>
+			<h1 className='Header'>Characters</h1>
+		</div>
+	);
 }
 
 export default App;
+
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
+
+// https://swapi.py4e.com/api/people/
